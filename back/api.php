818e69bee3b12c55ad0eefd137ca8320ec974d3d
Allan Cueff <allan.cueff@protonmail.com>
2025-04-30 12:25:16 +0200

    test cors

--- back/api.php
+++ back/api.php
@@ -1,5 +1,15 @@
 <?php
-    require_once 'utils/database.php';
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    # Si c'est une requête OPTIONS
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
+    require_once 'database.php';
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
 
