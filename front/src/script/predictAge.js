8aca6e3a0960ba60cb43b7a91ebde79fb20b6ea4
Allan Cueff <allan.cueff@protonmail.com>
2025-05-02 11:44:41 +0200

    map de l'arbre embed

--- front/src/script/predictAge.js
+++ front/src/script/predictAge.js
@@ -1,38 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get("id");
-  
-    if (id) {
-      console.log("ID récupéré :", id);
-  
-      getTreeById(id, function (tree) {
-        let speciesName = tree.species.name;
-        let stateName = tree.state.name;
-        let devName = tree.dev.name;
-        let portName = tree.port.name;
-        let piedName = tree.pied.name;
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
 
-        let totalHeight = tree.totalHeight;
-        let trunkDiameter = tree.trunkDiameter;
-        let trunkHeight = tree.trunkHeight;
+  if (id) {
+    console.log("ID récupéré :", id);
 
-        console.log("taille totale : " + totalHeight + "taille tronc : " + trunkDiameter + "diamètre tronc : " + trunkDiameter);
-        let lat = tree.lat;
-        let lon = tree.long;
-        let age = tree.age;
-        let isRemarkable = tree.isRemarkable;
+    getTreeById(id, function (tree) {
+      let speciesName = tree.species.name;
+      let stateName = tree.state.name;
+      let devName = tree.dev.name;
+      let portName = tree.port.name;
+      let piedName = tree.pied.name;
 
-        document.getElementById("id").textContent = id;
-        document.getElementById("speciesName").textContent = speciesName;
+      let totalHeight = tree.totalHeight;
+      let trunkDiameter = tree.trunkDiameter;
+      let trunkHeight = tree.trunkHeight;
 
-        predictAge(totalHeight, trunkDiameter, trunkHeight, function(response){
-            const predictedAge = response.value; 
-            console.log(predictedAge);
-            document.getElementById("ageEstimeBest").textContent = "Age estimé par le meilleur modèle : " + predictedAge + " ans";
-        })
+      console.log("taille totale : " + totalHeight + "taille tronc : " + trunkDiameter + "diamètre tronc : " + trunkDiameter);
+      let lat = tree.lat;
+      let lon = tree.long;
+      let age = tree.age;
+      let isRemarkable = tree.isRemarkable;
 
-        });
-    } else {
-      alert("Aucun ID trouvé dans l'URL !");
-    }
-  });
+      document.getElementById("id").textContent = id;
+      document.getElementById("speciesName").textContent = speciesName;
+
+      let googleMapsUrl = `https://www.google.com/maps/@${lat},${lon},15z`;
+      let googleMapEmbedUrl = `https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`
+      document.getElementById('ggmap').setAttribute('href', googleMapsUrl);
+      document.getElementById('ggmapiframe').setAttribute('src', googleMapEmbedUrl);
+
+      predictAge(totalHeight, trunkDiameter, trunkHeight, function(response){
+          const predictedAge = response.value; 
+          console.log(predictedAge);
+          document.getElementById("ageEstimeBest").textContent = "Age estimé par le meilleur modèle : " + predictedAge + " ans";
+      })
+
+      });
+  } else {
+    alert("Aucun ID trouvé dans l'URL !");
+  }
+});
